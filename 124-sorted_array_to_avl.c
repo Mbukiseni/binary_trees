de571fab2085e655999fc76ac752703556440fb6
Mbukiseni <mbndima@gmail.com>
2023-11-28 00:27:33 -0800

    124-sorted_array_to_avl.c

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,34 +1,50 @@
 #include "binary_trees.h"
 
 /**
- * create_tree - creates the AVL tree with recursion
+ * sorted_array_to_avl - builds AVL tree from the array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of elements in the array
  *
- * @node: The pointer node
- * @array: The input array of integers
- * @size: The size of array
- * @mode: 1 to adding on the left, 2 to adding on the right
- * Return: no return
+ * Return: pointer to the root node of the created AVL tree
+ *         NULL on failure
  */
-void create_tree(avl_t **node, int *array, size_t size, int mode)
+avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	avl_t *tree = NULL;
 	size_t middle;
 
-	if (size == 0)
-		return;
+	if (!array)
+		return (NULL);
+	middle = (size - 1) / 2;
+	tree = binary_tree_node(NULL, array[middle]);
 
-	middle = (size / 2);
-	middle = (size % 2 == 0) ? middle - 1 : middle;
+	sata_helper(&tree, array, -1, middle);
+	sata_helper(&tree, array, middle, size);
 
-	if (mode == 1)
+	return (tree);
+}
+
+/**
+ * sata_helper - helper that builds an AVL tree from the array
+ * @root: double pointer to the root node of the subtree
+ * @array: pointer to the first element of the array to be converted
+ * @lo: the lower bound index
+ * @hi: the upper bound index
+ */
+void sata_helper(avl_t **root, int *array, size_t lo, size_t hi)
+{
+	avl_t *new = NULL;
+	size_t middle;
+
+	if (hi - lo > 1)
 	{
-		(*node)->left = binary_tree_node(*node, array[middle]);
-		create_tree(&((*node)->left), array, middle, 1);
-		create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
-	}
-	else
-	{
-		(*node)->right = binary_tree_node(*node, array[middle]);
-		create_tree(&((*node)->right), array, middle, 1);
-		create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+		middle = (hi - lo) / 2 + lo;
+		new = binary_tree_node(*root, array[middle]);
+		if (array[middle] > (*root)->n)
+			(*root)->right = new;
+		else if (array[middle] < (*root)->n)
+			(*root)->left = new;
+		sata_helper(&new, array, lo, middle);
+		sata_helper(&new, array, middle, hi);
 	}
 }
