4736c40fc47d8609e4b498e26078b7d0c23ada84
Mbukiseni <mbndima@gmail.com>
2023-11-27 23:52:03 -0800

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,113 +1,56 @@
 #include "binary_trees.h"
 
 /**
- * struct node_s - The singly linked list
- * @node: const binary tree node
- * @next: points to next node
- */
-typedef struct node_s
-{
-	const binary_tree_t *node;
-	struct node_s *next;
-} ll;
-
-ll *append(ll *head, const binary_tree_t *btnode);
-void free_list(ll *head);
-ll *get_children(ll *head, const binary_tree_t *parent);
-void levels_rec(ll *head, void (*func)(int));
-
-/**
- * binary_tree_levelorder - Goes through binary tree
- *                          using level-order traversal.
- * @tree: Pointer to root node of the tree to traverse.
- * @func: Pointer to function to call for each node.
+ * binary_tree_levelorder - traverst binary tree using level-order traverse
+ * @tree: the tree to traverse
+ * @func: the pointer to function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	ll *children = NULL;
+	size_t level, maxlevel;
 
-	func(tree->n);
-	children = get_children(children, tree);
-	levels_rec(children, func);
-
-	free_list(children);
-}
-
-/**
- * levels_rec - Calls function on all nodes at each level.
- * @head: Pointer to head of linked list with nodes at one level.
- * @func: Pointer to function to call for each node.
- */
-void levels_rec(ll *head, void (*func)(int))
-{
-	ll *children = NULL, *curr = NULL;
-
-	if (!head)
+	if (!tree || !func)
 		return;
-	for (curr = head; curr != NULL; curr = curr->next)
-	{
-		func(curr->node->n);
-		children = get_children(children, curr->node);
-	}
-	levels_rec(children, func);
-	free_list(children);
+
+	maxlevel = binary_tree_height(tree) + 1;
+
+	for (level = 1; level <= maxlevel; level++)
+		btlo_helper(tree, func, level);
 }
 
 /**
- * get_children - Appends children of parent to linked list.
- * @head: Pointer to head of linked list where children will be appended.
- * @parent: Pointer to node whose children we want to append.
- * Return: Pointer to the head of linked list of children.
+ * btlo_helper - goes through binary tree using post-order traverse
+ * @tree: the tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level:level of the tree to call func upon
  */
-ll *get_children(ll *head, const binary_tree_t *parent)
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
 {
-	if (parent->left)
-		head = append(head, parent->left);
-	if (parent->right)
-		head = append(head, parent->right);
-	return (head);
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
 }
 
 /**
- * append - adds new node at the end of linkedlist
- * @head: pointer to head of linked list
- * @btnode: const binary tree node to append
- * Return: pointer to head, or NULL on failure
+ * binary_tree_height - measures the height of the binary tree
+ * @tree: the tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
  */
-ll *append(ll *head, const binary_tree_t *btnode)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	ll *new = NULL, *last = NULL;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-	new = malloc(sizeof(*new));
-	if (new)
-	{
-		new->node = btnode;
-		new->next = NULL;
-		if (!head)
-			head = new;
-		else
-		{
-			last = head;
-			while (last->next)
-				last = last->next;
-			last->next = new;
-		}
-	}
-	return (head);
-}
+	if (!tree)
+		return (0);
 
-/**
- * free_list - frees all the nodes in linked list
- * @head: pointer to head of list_t linked list
- */
-void free_list(ll *head)
-{
-	ll *ptr = NULL;
-
-	while (head)
-	{
-		ptr = head->next;
-		free(head);
-		head = ptr;
-	}
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
